aed45981c421886a7b9cc4e36585359bd0031f9f
luiz-avelar <luiz.avelar25@gmail.com>
2020-07-27 18:49:07 -0300

    Codeclimate corrections

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -3,22 +3,18 @@ import * as api from '../services/api';
 
 class Categories extends React.Component {
   constructor(props) {
-    super(props)
-    this.state = {
-      categories: [],
-    }
+    super(props);
+    this.state = { categories: [] };
     this.updateCategories = this.updateCategories.bind(this);
   }
-  
+
   async componentDidMount() {
     const categories = await api.getCategories();
     this.updateCategories(categories);
   }
 
   updateCategories(newCategories) {
-    this.setState({
-      categories: newCategories, 
-    })
+    this.setState({ categories: newCategories });
   }
 
   render() {
@@ -28,15 +24,8 @@ class Categories extends React.Component {
       <select>
         {categories.map((category) => <option key={category.id}>{category.name}</option>)}
       </select>
-    )
+    );
   }
 }
 
 export default Categories;
-
-// 2. Criar página de listagem de produtos vazia
-// A tela básica da plataforma é a tela de listagem de produtos, onde quem usa buscará o que quer para adicionar ao carrinho e filtrará suas buscas.
-
-// Esta página deve ficar no path /.
-// Mostre a mensagem "Digite algum termo de pesquisa ou escolha uma categoria.".
-// Adicione o atributo data-testid com o valor home-initial-message no elemento da mensagem.
