e382e7bab3d77eaec1bf7dc3287e841c0b323057
Kyle Felipe Vieira Roberto <kylefelipe@gmail.com>
2020-07-27 19:11:54 -0300

    Importando o search.

--- src/pages/ProductList.js
+++ src/pages/ProductList.js
@@ -1,32 +1,38 @@
 import React, { Component } from 'react';
+import Search from '../components/Search';
+import './ProductList.css';
 
 class MovieList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       products: undefined,
-      loading: true,
+      searchText: undefined,
     };
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+  
+  handleSearch(event) {
+    const text = event.target.value;
+    this.setState({searchText: text})
   }
 
   render() {
-    const { products, loading } = this.state;
+    const { products } = this.state;
 
-    if (!products) {
-      return (
-        <div>
-          <div className="category-container">
-            categorias
-          </div>
-          <div>
-            barra de pesquisa
-          </div>
-          <div>
-            Nenhum produto foi encontrado
-          </div>
+    return (
+      <div className="productlist-container">
+        <div className="category-container">
+          categorias
         </div>
-      );
-    }
+        <div className="search-container">
+          <Search onClick={this.handleSearch} />
+        </div>
+        <div>
+          Nenhum produto foi encontrado
+        </div>
+      </div>
+    );
   }
 }
 
