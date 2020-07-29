e261f4702e7f9d914890e25f843238f51368a107
ferrari <luisdiasferrari@gmail.com>
2020-07-29 15:31:53 -0300

    item 5

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,49 +2,52 @@ import React, { Component } from 'react';
 import Categories from '../components/Categories';
 import './Header.css';
 
-export default class Pesquisa extends Component {
-  constructor() {
-    super();
-    this.state = { searchText: '' };
+export default class Search extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchText: '',
+      products: [],
+    };
+    console.log(this.state.searchText)
     this.newData = this.newData.bind(this);
-    this.selctItem = this.selctItem.bind(this);
   }
-  newData(element) {
-    const { name, value } = element.target;
-    this.setState({ [name]: value });
-  }
+
   busca() {
     return (
       <div className="busca">
         <label htmlFor="inputBusca" data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.</label><br />
         <input
+          data-testid="query-input"
           className="inputBusca"
           name="searchTest"
           type="text"
-          value={this.state.searchText}
           onChange={this.newData}
         /><br />
         <Categories />
       </div>
     );
   }
-  selctItem() {
-    const onClick = this.props.onClick;
-    onClick(this.state);
+
+  newData(event) {
+    const text = event.target.value;
+    this.setState({ searchText: text });
   }
+
   botao() {
     return (
       <button
         data-testid="query-button"
         className="botao"
         type="submit"
-        onClick={this.selctItem}
+        onClick={this.detProductor}
       >
       Pesquisar
       </button>
     );
   }
+
   render() {
     return (
       <form className="search">
--- src/page/List.js
+++ src/page/List.js
@@ -3,22 +3,29 @@ import { Link } from 'react-router-dom';
 import Search from '../components/Search';
 import cartImg from '../images/cart.png';
 import logoImg from '../images/logo.png';
+import * as api from '../services/api';
 import './List.css';
 
 export default class List extends Component {
   constructor() {
     super();
-    this.onClick = this.onClick.bind(this);
+    this.state = {
+      Products: [],
+    }
+    this.detProductor = this.detProductor.bind(this);
   }
-  onClick(event) {
-    this.setState({ searchText: event.target.value });
+
+  async detProductor(searchText) {
+    await api.getProductsFromCategoryAndQuery(this.state.categoryId, searchText)
+    .then((data) => this.setState({ products: data.results }));
   }
+
   render() {
     return (
       <div>
         <header>
           {<img className="logoImg" src={logoImg} alt="Logo" />}
-          <Search onClick={this.onClick} />
+          <Search />
           <Link to="/cart" className="shopping-cart-button">
             {<img
               className="cartImg" data-testid="shopping-cart-button"
@@ -26,6 +33,9 @@ export default class List extends Component {
             />}
           </Link>
         </header>
+        <div>
+
+        </div>      
       </div>
     );
   }
