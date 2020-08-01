6a42136cb1542b0a393cb1e922f16d40d4c8ec1f
ferrari <luisdiasferrari@gmail.com>
2020-08-01 10:29:08 -0300

    item5

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -11,7 +11,8 @@ export default class ProductCard extends Component {
       >
         <img className="card-image" src={product.thumbnail} alt={product.title} />
         <h4 className="card-title">{product.title}</h4>
-        <p className="card-price">{product.price}</p>
+        <p className="card-price" onkeyup="maskIt(this,event,'###.###.###,##',true,{pre:'R$'})
+">{product.price}</p>
       </div>
     );
   }
