b605e9ecefa2fa10a15c97c38435540000d84e5c
luiz-avelar <luiz.avelar25@gmail.com>
2020-08-01 13:11:12 -0300

    Requirement 7 completed and new api call added

--- src/pages/ProductDetails.js
+++ src/pages/ProductDetails.js
@@ -7,13 +7,17 @@ class ProductDetails extends React.Component {
     this.state = {
       loading: true,
       product: null,
+      newComment: '',
+      commentList: [],
     };
     this.updateState = this.updateState.bind(this);
+    this.addComment = this.addComment.bind(this);
+    this.updateCommentList = this.updateCommentList.bind(this);
   }
 
   async componentDidMount() {
-    const itemID = this.props.match.params.id;
-    const response = await fetch(`https://api.mercadolibre.com/items/${itemID}`);
+    const itemId = this.props.match.params.id;
+    const response = await fetch(`https://api.mercadolibre.com/items/${itemId}`);
     const data = await response.json();
     this.updateState(data);
   }
@@ -25,15 +29,40 @@ class ProductDetails extends React.Component {
     });
   }
 
+  addComment() {
+    this.setState((prevState) => ({
+      commentList: [...prevState.commentList, prevState.newComment],
+      newComment: '',
+    }));
+  }
+
+  updateCommentList(event) {
+    this.setState({ newComment: event.target.value });
+  }
+
   render() {
-    const { loading, product } = this.state;
+    const { loading, product, newComment, commentList } = this.state;
+
     if (loading) return <h1>Carregando...</h1>;
 
     return (
       <div>
-        <h1 data-testid="product-detail-name">{product.title}</h1>
-        <img src={product.thumbnail} alt="product thumbnail" />
-        <h2>{product.price}</h2>
+        <div>
+          <h1 data-testid="product-detail-name">{product.title}</h1>
+          <img src={product.thumbnail} alt="product thumbnail" />
+          <h2>{product.price}</h2>
+        </div>
+        <div>
+          <label htmlFor="commentBar">Escreva um comentário sobre o produto:</label>
+          <input
+            data-testid="product-detail-evaluation" type="text" id="commentBar"
+            value={newComment} onChange={this.updateCommentList}
+          />
+          <button type="submitt" onClick={this.addComment}>Enviar</button>
+          <ul>
+            {commentList.map((comment, index) => <li key={`${index + 1}`}>{comment}</li>)}
+          </ul>
+        </div>
         <Link to="/">Voltar</Link>
       </div>
     );
