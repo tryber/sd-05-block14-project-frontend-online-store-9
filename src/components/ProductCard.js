13dbe5b452625999f659858cb6d99b70d521f62a
luiz-avelar <luiz.avelar25@gmail.com>
2020-07-31 22:20:23 -0300

    Requirement 7 completed

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './ProductCard.css';
+import { Link } from 'react-router-dom';
 
 export default class ProductCard extends Component {
   render() {
@@ -12,7 +13,9 @@ export default class ProductCard extends Component {
         <img className="card-image" src={product.thumbnail} alt={product.title} />
         <h4 className="card-title">{product.title}</h4>
         <p className="card-price">{product.price}</p>
+        <Link to={`/productDetails/${product.id}`} data-testid="product-detail-link">Ver detalhes</Link>
       </div>
     );
   }
 }
+
